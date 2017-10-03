dfe8108b929911635f018e00dd0763b0f275180a
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-03 13:18:26 -0600

    Created Kahoot quizzes and added them to the list.

--- src/tools/controller/ListController.java
+++ src/tools/controller/ListController.java
@@ -1,10 +1,35 @@
 package tools.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import tools.model.Kahoot;
+
 public class ListController
 {
-
+	private List<Kahoot> myKahoots;
+	
+	public ListController()
+	{
+		myKahoots = new ArrayList<Kahoot>();
+	}
+	
 	public void start()
 	{
-		
+		Kahoot myFirstKahoot = new Kahoot();
+		myKahoots.add(myFirstKahoot);
+	}
+	
+	private void fillTheList()
+	{
+		Kahoot fiftyStates = new Kahoot("Kashish", 50);
+		Kahoot mySecondKahoot = new Kahoot("Ethan", 2);
+		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);
+		Kahoot animalColor = new Kahoot("Branton", 10);
+		Kahoot transformersMovie = new Kahoot("AJ", 5) ;
+		myKahoots.add(fiftyStates);
+		myKahoots.add(mySecondKahoot);
+		myKahoots.add(bigQuiz);
+		myKahoots.add(animalColor);
+		myKahoots.add(transformersMovie);
 	}
 }
