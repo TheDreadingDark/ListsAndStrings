1098def7c191e3efc141f5560650f6c46a99fe55
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-03 13:48:46 -0600

    Added popup to show quizzes.

--- src/tools/controller/ListController.java
+++ src/tools/controller/ListController.java
@@ -3,20 +3,33 @@ package tools.controller;
 import java.util.List;
 import java.util.ArrayList;
 import tools.model.Kahoot;
+import tools.view.PopupDisplay;
 
 public class ListController
 {
 	private List<Kahoot> myKahoots;
+	private PopupDisplay popup;
 	
 	public ListController()
 	{
 		myKahoots = new ArrayList<Kahoot>();
+		popup = new PopupDisplay();
 	}
 	
 	public void start()
 	{
 		Kahoot myFirstKahoot = new Kahoot();
 		myKahoots.add(myFirstKahoot);
+		fillTheList();
+		showTheList();
+	}
+	
+	private void showTheList()
+	{
+		for (int index = 0; index < myKahoots.size(); index += 1)
+		{
+			popup.displayText(myKahoots.get(index).toString());
+		}
 	}
 	
 	private void fillTheList()
