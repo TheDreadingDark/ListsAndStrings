c246627f42ff5570cf7a80e16e6df5fbbcbd9b5d
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-05 13:18:20 -0600

    Changed constructor perameters, added a data member.

--- src/tools/controller/ListController.java
+++ src/tools/controller/ListController.java
@@ -26,19 +26,39 @@ public class ListController
 	
 	private void showTheList()
 	{
+		String currentCreator = "";
 		for (int index = 0; index < myKahoots.size(); index += 1)
 		{
+			currentCreator = myKahoots.get(index).getCreator();
+			
+			Kahoot currentKahoot = myKahoots.get(index);
+			String creator = currentKahoot.getCreator();
+			
 			popup.displayText(myKahoots.get(index).toString());
+			
+			if (currentCreator.equals("nobody"))
+			{
+				for (int loop = 0; loop < 5; loop += 1)
+				{
+					popup.displayText("wow nobody does a lot");
+				}
+			}
+		
+			for (int currentLetterIndex = 0; currentLetterIndex < currentCreator.length(); currentLetterIndex += 1)
+			{
+				popup.displayText(currentCreator.substring(currentLetterIndex,  currentLetterIndex + 1));
+			}
+		
 		}
 	}
 	
 	private void fillTheList()
 	{
-		Kahoot fiftyStates = new Kahoot("Kashish", 50);
-		Kahoot mySecondKahoot = new Kahoot("Ethan", 2);
-		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);
-		Kahoot animalColor = new Kahoot("Branton", 10);
-		Kahoot transformersMovie = new Kahoot("AJ", 5) ;
+		Kahoot fiftyStates = new Kahoot("Kashish", 50, "The fifty United States");
+		Kahoot mySecondKahoot = new Kahoot("Ethan", 2, "The Double data type");
+		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE, "Everything - literally");
+		Kahoot animalColor = new Kahoot("Branton", 10, "All the colors of the animals");
+		Kahoot transformersMovie = new Kahoot("AJ", 5, "The 1986 Transformers Movie");
 		myKahoots.add(fiftyStates);
 		myKahoots.add(mySecondKahoot);
 		myKahoots.add(bigQuiz);
