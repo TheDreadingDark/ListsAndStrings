c99061f6ded822b496f6c0f3d90f0b856f8694d4
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-10-09 12:24:29 -0600

    Did the add portion and Add(new Kahoot(param).

--- src/tools/controller/ListController.java
+++ src/tools/controller/ListController.java
@@ -22,42 +22,44 @@ public class ListController
 		myKahoots.add(myFirstKahoot);
 		fillTheList();
 		showTheList();
+		changeTheList();
+		ListsPrac();
 	}
 	
 	private void showTheList()
+{
+	String currentCreator = "";
+	for (int index = 0; index < myKahoots.size(); index += 1)
 	{
-		String currentCreator = "";
-		for (int index = 0; index < myKahoots.size(); index += 1)
+		currentCreator = myKahoots.get(index).getCreator();
+			
+		Kahoot currentKahoot = myKahoots.get(index);
+		String creator = currentKahoot.getCreator();
+			
+		popup.displayText(myKahoots.get(index).toString());
+			
+		if (currentCreator.equals("nobody"))
 		{
-			currentCreator = myKahoots.get(index).getCreator();
-			
-			Kahoot currentKahoot = myKahoots.get(index);
-			String creator = currentKahoot.getCreator();
-			
-			popup.displayText(myKahoots.get(index).toString());
-			
-			if (currentCreator.equals("nobody"))
+			for (int loop = 0; loop < 5; loop += 1)
 			{
-				for (int loop = 0; loop < 5; loop += 1)
-				{
-					popup.displayText("wow nobody does a lot");
-				}
+				popup.displayText("wow nobody does a lot");
 			}
-		
-			for (int currentLetterIndex = 0; currentLetterIndex < currentCreator.length(); currentLetterIndex += 1)
-			{
-				popup.displayText(currentCreator.substring(currentLetterIndex,  currentLetterIndex + 1));
-			}
-			
-			String topic = currentKahoot.getTopic();
-			
-			for (int letter = currentKahoot.getTopic().length() - 1; letter >= 0; letter -= 1)
-			{
-				popup.displayText(topic.substring(letter, letter + 1));
-			}
-		
 		}
+		
+		for (int currentLetterIndex = 0; currentLetterIndex < currentCreator.length(); currentLetterIndex += 1)
+		{
+			popup.displayText(currentCreator.substring(currentLetterIndex,  currentLetterIndex + 1));
+		}
+			
+		String topic = currentKahoot.getTopic();
+			
+		for (int letter = currentKahoot.getTopic().length() - 1; letter >= 0; letter -= 1)
+		{
+			popup.displayText(topic.substring(letter, letter + 1));
+		}
+		
 	}
+}
 	
 	private void fillTheList()
 	{
@@ -72,4 +74,23 @@ public class ListController
 		myKahoots.add(animalColor);
 		myKahoots.add(transformersMovie);
 	}
+		
+	private void changeTheList()
+	{
+		popup.displayText("The current list size is: " + myKahoots.size());
+		Kahoot removed  = myKahoots.remove(3);
+		popup.displayText("I removed the Kahoot by " + removed.getCreator());
+		popup.displayText("The list is now has: " + myKahoots.size() + "items inside.");
+		myKahoots.add(0, removed);
+		
+		popup.displayText("The list is still: " + myKahoots.size() + " items big.");
+		removed = myKahoots.set(2, new Kahoot());
+		popup.displayText("The kahoot by " + removed.getCreator() + " was replaced with on by: " + myKahoots.get(2).getCreator());
+	}
+		
+	private void ListsPrac()
+	{
+		myKahoots.add(0, new Kahoot("Dave", 8, "Plants"));
+		
+	}	
 }
